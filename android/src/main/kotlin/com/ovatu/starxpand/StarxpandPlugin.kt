c30df0177d649660d4af221a5dc325684b22e151
Adam Viðarsson <adam@igital.co>
2023-11-02 14:56:20 +0000

    OnTime 1

--- android/src/main/kotlin/com/ovatu/starxpand/StarxpandPlugin.kt
+++ android/src/main/kotlin/com/ovatu/starxpand/StarxpandPlugin.kt
@@ -334,7 +334,7 @@ class StarxpandPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
       "no2" -> Channel.No2
       else -> Channel.No1
     }
-    return DrawerBuilder().actionOpen(OpenParameter().setChannel(channel).setOnTime(0))
+    return DrawerBuilder().actionOpen(OpenParameter().setOnTime(1).setChannel(channel))
   }
 
   private fun getPrinterBuilder(data: Map<*, *>): PrinterBuilder {
